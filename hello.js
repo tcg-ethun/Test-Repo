1dda2c318c272c9843bec9deea6677a1d13e2d6f
Esan <ethuntcg@gmail.com>
2025-03-14 23:29:20 +0600

    Update

--- hello.js
+++ hello.js
@@ -1,7 +1,14 @@
 
 let element = document.getElementById("span");
 
-function clickED(){
-let element = document.getElementById("span");
-    element.style.color ="blue";
+function darkMode(){
+let dark = document.getElementById("body");
+ dark.style.backgroundColor ="black";
+ dark.style.color = 'White';
+}
+
+function lightMode(){
+    let light = document.getElementById("body");
+ light.style.backgroundColor = "white";
+ light.style.color = 'black';
 }
