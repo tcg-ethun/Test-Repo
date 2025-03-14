378b116ac93d41e120683d513552e9b423e7f7d1
Esan <ethuntcg@gmail.com>
2025-03-14 22:00:44 +0600

    Update

---
+++ hello.js
@@ -0,0 +1,8 @@
+
+let element = document.getElementById("span");
+
+function click(){
+let element = document.getElementsByTagName("span");
+    element.style.color ="blue";
+    console.log("Haha")
+}
